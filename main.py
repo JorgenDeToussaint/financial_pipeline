0a77998e8c90837fa70d7aa704a8472495a04e4a
JorgenDeToussaint <guydoingsomethings@tutanota.com>
2026-02-01 11:16:23 +0100

    fix: corrected path for main.py in docker-compose

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from src.extractors.extractor_template import extract
-from src.loaders.s3_gecko_loader import upload_to_minio
+from src.loaders.s3_gecko_loader import s3_loader  # Importujemy klasę, nie funkcję
 from datetime import datetime
 
-
-
 if __name__ == "__main__":
+    # 1. Pobieramy dane
     dane = extract()
     print(f"Main odebrał {len(dane)} rekordów.")
+
+    # 2. Jeśli mamy dane, wysyłamy
     if dane:
-        upload_to_minio(dane, bucket="raw-data", instrument="stable-coins")
+        # Generujemy timestamp raz dla całego procesu
+        now = datetime.now()
+        
+        # Inicjalizujemy loader (Upewnij się, że te dane pasują do Twojego MinIO)
+        loader = s3_loader(
+            endpoint_url="http://localhost:9000", 
+            access_key="minioadmin", 
+            secret_key="minioadmin"
+        )
+        
+        # Wywołujemy metodę wysyłki
+        loader.upload_raw_json(
+            data=dane, 
+            bucket="raw-data", 
+            instrument="stable-coins", 
+            timestamp=now
+        )
