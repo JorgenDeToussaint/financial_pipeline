51ecff53a327089d908416ebb03308ba94d088ad
JorgenDeToussaint <guydoingsomethings@tutanota.com>
2026-02-01 11:16:24 +0100

    fix: logging implemented in main, extractor and loader

--- src/extractors/extractor_template.py
+++ src/extractors/extractor_template.py
@@ -2,6 +2,10 @@ import requests
 import os
 import json
 from datetime import datetime
+from src.utils.logger import get_logger
+
+
+logger = get_logger(__name__)
 
 def extract(timestamp: datetime):
     url = "https://api.coingecko.com/api/v3/coins/markets"
@@ -16,16 +20,18 @@ def extract(timestamp: datetime):
     ts_str = timestamp.strftime("%Y%m%d_%H%M%S")
 
     try:
+        logger.info(f"Rozpoczynam pobieranie z: {url}")
         r = requests.get(url, params=query_params) 
         r.raise_for_status()
+        logger.debug(f"Status odpowiedzi: {r.status_code}")
         data = r.json()
     
-        # Backup lokalny
+        
         os.makedirs("data/raw", exist_ok=True)
         with open(f"data/raw/stable_coins_{ts_str}.json", "w") as f:
             json.dump(data, f, indent=4)
         
         return data
     except Exception as e:
-        print(f"Krytyczny błąd ekstrakcji: {e}!")
+        logger.error(f"Nie udało się pobrać danych: {e}")
         return []
--- src/loaders/s3_gecko_loader.py
+++ src/loaders/s3_gecko_loader.py
@@ -2,7 +2,10 @@ import boto3
 import json
 from datetime import datetime
 from botocore.exceptions import EndpointConnectionError
+from src.utils.logger import get_logger
 
+
+logger = get_logger(__name__)
 class s3_loader:
     def __init__(self, endpoint_url, access_key, secret_key):
         self.s3 = boto3.client(
@@ -13,31 +16,33 @@ class s3_loader:
         )
 
     def upload_raw_json(self, data: list, bucket: str, instrument: str, timestamp: datetime):
-        # 1. Budujemy ścieżkę (z :02d dla porządku w MinIO)
         partition_path = (
             f"instrument={instrument}/"
             f"year={timestamp.year}/"
             f"month={timestamp.month:02d}/"
             f"day={timestamp.day:02d}"
         )
+
         file_name = f"{instrument}_{timestamp.strftime('%H%M%S')}.json"
         full_key = f"raw/{partition_path}/{file_name}"
 
         try:
-            # KLUCZOWY MOMENT: Zamiana listy na bajty
+            logger.info(f"Rozpoczynam ładowanie danych do bucketu: {bucket} (Key: {full_key})")
             json_string = json.dumps(data, indent=4)
             json_bytes = json_string.encode('utf-8')
 
             self.s3.put_object(
                 Bucket=bucket,
                 Key=full_key,
-                Body=json_bytes # Wysyłamy BAJTY, nie listę!
+                Body=json_bytes 
             )
             print(f"Pomyślnie wysłano: s3://{bucket}/{full_key}")
             return True
-        except EndpointConnectionError:
-            print("[!] Błąd połączenia z MinIO!")
+        
+        except EndpointConnectionError as e:  # <--- Dodaj 'as e'
+            logger.error(f"❌ Błąd połączenia z S3: {str(e)}")
             return False
         except Exception as e:
-            print(f"[!] Błąd Loadera: {e}")
+            logger.error(f"❌ Nieoczekiwany błąd: {str(e)}") # <--- Zaloguj błąd, nie tylko payload!
+            logger.debug(f"Payload błędu: {data}")
             return False
