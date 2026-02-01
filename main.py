7727974976d3f533062c1bc7bf70e196588fe982
JorgenDeToussaint <guydoingsomethings@tutanota.com>
2026-02-01 11:16:23 +0100

    fix: added loader to main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from src.extractors.extractor_template import extract
+from src.loaders.s3_gecko_loader import upload_to_minio
+from datetime import datetime
+
+
 
 if __name__ == "__main__":
     dane = extract()
     print(f"Main odebrał {len(dane)} rekordów.")
-
+    if dane:
+        upload_to_minio(dane, bucket="raw-data", instrument="stable-coins")
