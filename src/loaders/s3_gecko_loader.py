6047df08a1c866e9284ac99312cf26737874ebee
JorgenDeToussaint <guydoingsomethings@tutanota.com>
2026-02-01 11:16:24 +0100

    fix: now loader works, correctly puted json into minio3 on two different attempts

--- src/loaders/s3_gecko_loader.py
+++ src/loaders/s3_gecko_loader.py
@@ -13,31 +13,31 @@ class s3_loader:
         )
 
     def upload_raw_json(self, data: list, bucket: str, instrument: str, timestamp: datetime):
-        #Hive-style path
+        # 1. Budujemy ścieżkę (z :02d dla porządku w MinIO)
         partition_path = (
             f"instrument={instrument}/"
             f"year={timestamp.year}/"
-            f"month={timestamp.month}/"
-            f"day={timestamp.day}/"
+            f"month={timestamp.month:02d}/"
+            f"day={timestamp.day:02d}"
         )
-        #filename convention
         file_name = f"{instrument}_{timestamp.strftime('%H%M%S')}.json"
         full_key = f"raw/{partition_path}/{file_name}"
 
         try:
-            json_data = json.dumps(data, indent=4)
+            # KLUCZOWY MOMENT: Zamiana listy na bajty
+            json_string = json.dumps(data, indent=4)
+            json_bytes = json_string.encode('utf-8')
+
             self.s3.put_object(
                 Bucket=bucket,
                 Key=full_key,
-                Body=json_data
+                Body=json_bytes # Wysyłamy BAJTY, nie listę!
             )
             print(f"Pomyślnie wysłano: s3://{bucket}/{full_key}")
             return True
-        
         except EndpointConnectionError:
-            print("[!] Błąd: Nie można połączyć się z MinIO. Czy kontener działa?")
+            print("[!] Błąd połączenia z MinIO!")
             return False
-        
         except Exception as e:
-            print(f"[!] Nieoczekiwany błąd Loadera: {e}")
+            print(f"[!] Błąd Loadera: {e}")
             return False
